7c314aef5742b9049a09e4d181e7cbe51f790a89
quockunss <72240212+quockunss@users.noreply.github.com>
2020-11-02 15:48:16 +0700

    add index

---
+++ Number.js
@@ -0,0 +1,91 @@
+class Number{
+    constructor(number,arr) {
+        this.number = number;
+        this.arr = arr;
+    }
+
+    inputN(){
+        let num;
+        do {
+            num = +prompt('Nhập số N: ');
+        } while (num % 2 === 0 || num < 0);
+        this.number = num;
+    }
+
+    checkPrime(num){
+        let i = 2;
+        if (num < 2){
+            return false;
+        } else if ( num > 2){
+            for (let i = 2; i <= Math.sqrt(num); i++){
+                if (num % i == 0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    listPrime(){
+        let count = 0;
+        let i = 0;
+        while (count < this.number){
+            if (this.checkPrime(i) == true){
+                document.getElementById("128").innerHTML += i + ' ';
+                count++;
+            }
+            i++;
+        }
+    }
+
+    createarr(arr){
+        this.arr = arr
+    }
+
+
+    inputArray(){
+        for ( let i = 0 ; i < this.number; i++){
+            this.arr[i] = +prompt('nhap phan tu mang: ');
+        }
+    }
+
+
+    checkPrimeArr(){
+        for (let i = 0 ; i < this.arr.length; i++){
+            if (this.checkPrime(this.arr[i])){
+                document.getElementById('primearr').innerHTML += this.arr[i];
+            }
+        }
+    }
+
+
+    checkN(){
+        for (let i = 0; i < this.arr.length; i++){
+            if (this.arr[i] == this.number){
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+let arr = new Array();
+let number1 = new Number()
+
+number1.createarr(arr);
+number1.inputN();
+number1.checkPrime(number1.number);
+number1.listPrime();
+number1.inputArray();
+
+
